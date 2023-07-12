49c39932a126dab921c2bd085005fcd5ff757e89
jagLas <jglasher@gmail.com>
2023-07-12 11:55:48 -0400

    added tests for new_move_positions

--- test/test_path_finder.py
+++ test/test_path_finder.py
@@ -17,3 +17,13 @@ class PathFinderPossibleMoves(unittest.TestCase):
         knight = KnightPathFinder((4,5))
         self.assertSetEqual(knight.get_valid_moves((0,0)), set([(1,2),(2,1)]))
         self.assertSetEqual(knight.get_valid_moves((7,7)), set([(5,6),(6,5)]))
+
+class PathfinderNewMovePosition(unittest.TestCase):
+    def test_should_return_possible_moves(self):
+        knight = KnightPathFinder((4,5))
+        self.assertSetEqual(knight.new_move_positions((5,4)), set([(6, 2), (4, 6), (4, 2), (7, 3), (3, 3), (6, 6), (7, 5), (3, 5)]))
+
+    def test_should_filter_considered_positions(self):
+        knight = KnightPathFinder((4,5))
+        knight._considered_positions = set([(4,6), (3,3)])
+        self.assertSetEqual(knight.new_move_positions((5,4)), set([(6, 2), (4, 2), (7, 3), (6, 6), (7, 5), (3, 5)]))
